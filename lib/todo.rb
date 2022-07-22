df440655eb510483e5d6cb2b1cf32ece7994c9fa
allengoh <allen1311@hotmail.com>
2022-07-22 11:36:45 +0100

    Created the todo methods

---
+++ lib/todo.rb
@@ -0,0 +1,34 @@
+class Todo
+
+  def initialize 
+    @todo_list = ["buy milk", "walk the dog", "wash clothes"] 
+  end
+
+  def add_task(task)
+    @todo_list << task
+  end
+
+  def list_task
+    return @todo_list
+  end
+
+  def complete_task(task)
+      #method to remove task from array
+    
+    @todo_list.each do |i|
+      if i.include? task
+        return"This task has been completed"
+      end 
+    end
+  end
+
+  def remove_task(task)
+    @todo_list.delete_if { |i| i.include? task}
+  end
+
+end
+
+todo = Todo.new 
+todo.add_task("go to gym")
+puts todo.complete_task("buy milk")
+
---
+++ spec/todo_spec.rb
@@ -0,0 +1,9 @@
+require "todo"
+
+RSpec.describe Todo do
+  it "adds a task" do
+  end
+
+  it "lists a task" do
+  end
+end
