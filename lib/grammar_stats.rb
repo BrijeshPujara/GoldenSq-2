38f91e4497b577d7c2823c70d1335c9d7fac2c96
Brijesh Pujara <pujara.brijesh1@gmail.com>
2022-07-22 10:36:52 +0100

    GS phase 2.5 completed

--- lib/grammar_stats.rb
+++ lib/grammar_stats.rb
@@ -1,24 +1,25 @@
 class GrammarStats
     def initialize
       # ...
+      @count_T = 0
+      @count_F = 0
     end
   
     def check(text) # text is a string
       # Returns true or false depending on whether the text begins with a capital
       # letter and ends with a sentence-ending punctuation mark.
+      fail "Please input a string" if text == "" || text.class == Integer
       @check_grammar = text
-      @count_T = 0
-      @count_F = 0
 
-    #   if check_grammar[0] == check_grammar[0].upcase && check_grammar[-1] == "!"
-    #     @count_T +=1
-    #     return true
-    #   else
-    #     @count_F +=1
-    #     return false
-    #   end
+      if @check_grammar[0] == @check_grammar[0].upcase && ["!", "?", ",", "."].include?(@check_grammar[-1])
+        @count_T +=1
+        return true
+      else
+        @count_F +=1
+        return false
+      end
 
-    true if @check_grammar[0] == @check_grammar[0].upcase && @check_grammar[-1] == "!" 
+    # true if @check_grammar[0] == @check_grammar[0].upcase && @check_grammar[-1] == "!" 
       
 
     end
@@ -29,9 +30,18 @@ class GrammarStats
       # "One! two Three! four five" 2 words passed / 5 words checked = 2/5 = 0.4 x 100 = 40
 
       
-    #   total = @count_T + @count_F
-    #   result = @count_T / total * 100
+    total = @count_T + @count_F
+    result = @count_T / total.to_f * 100
+    return result.to_i
 
 
     end
   end
+
+  grammar = GrammarStats.new
+  grammar.check("Hello World!")
+  grammar.check("Hello World!")
+  grammar.check("Hello World!")
+  grammar.check("Hello World!")
+  grammar.check("hello World")
+  puts grammar.percentage_good
--- spec/grammar_stats_spec.rb
+++ spec/grammar_stats_spec.rb
@@ -2,9 +2,54 @@ require "grammar_stats"
 
 RSpec.describe GrammarStats do
   context "whether the text begins with a capital letter and ends with punctuation mark" do
-    it "returns true or false" do
+    it "returns true" do
       grammar = GrammarStats.new
-      expect(grammar.check("Hello world!")).to eq true
+      expect(grammar.check("Hello world?")).to eq true
+    end
+  end
+
+  context "whether the text begins with a capital letter and ends with punctuation mark" do
+    it "returns false" do
+      grammar = GrammarStats.new
+      expect(grammar.check("hello world")).to eq false
+    end
+  end
+
+  context "when the user inputs an empty string" do
+    it "raises an error" do
+      grammar = GrammarStats.new
+      expect{ grammar.check("") }.to raise_error "Please input a string"   
+    end
+  end
+
+  context "when the user inputs nothing as an argument" do
+    it "raises an argument error" do
+      grammar = GrammarStats.new
+      expect{ grammar.check() }.to raise_error ArgumentError
+    end
+  end
+
+  context "when the user inputs an integer as an argument" do
+    it "raises an error" do
+      grammar = GrammarStats.new
+      expect{ grammar.check(1234) }.to raise_error "Please input a string"
+    end
+  end
+
+  context "whether it returns 100% after 1 text and 1 pass" do
+    it "returns 100" do
+      grammar = GrammarStats.new
+      grammar.check("Hello World!")
+      expect(grammar.percentage_good()).to eq 100
+    end
+  end
+
+  context "whether it returns 50% after 2 texts and 1 pass" do
+    it "returns 50" do
+      grammar = GrammarStats.new
+      grammar.check("Hello World!")
+      grammar.check("Hello World")
+      expect(grammar.percentage_good()).to eq 50
     end
   end
 end
